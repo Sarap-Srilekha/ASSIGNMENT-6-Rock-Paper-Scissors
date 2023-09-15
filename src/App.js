5108c43a1b0bc4b35d10ab14e4d4595087ffd45c
Sarap-Srilekha <sarapsrilekha@gmail.com>
2023-09-15 05:47:37 +0000

    Rock paper Scissors solution

---
+++ src/App.js
@@ -0,0 +1,31 @@
+/* import './App.css'
+
+const choicesList = [
+  {
+    id: 'ROCK',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rock-image.png',
+  },
+  {
+    id: 'SCISSORS',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/scissor-image.png',
+  },
+  {
+    id: 'PAPER',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/paper-image.png',
+  },
+]
+
+const App = () => <div>Hello World</div>
+
+export default App */
+
+import RpsPage from './Components/RpsPage'
+
+import './App.css'
+
+const App = () => <RpsPage />
+
+export default App
